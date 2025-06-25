e02767d14da5acfe01e9bac4f181165696bf6de4
Carlos Guilherme Matos de Almeida da Silva <carlos.guilherme@grupoboticario.com.br>
2025-06-25 12:09:08 -0300

    Ajuste na habilitação de API's

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -5,7 +5,6 @@ resource "google_project_service" "apis" {
     "containerregistry.googleapis.com",
     "redis.googleapis.com",
     "iam.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
     "servicemanagement.googleapis.com",
     "serviceusage.googleapis.com",
   ])
